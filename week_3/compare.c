4ac8bf249debb294b19105bb857b2d2f4453d5c6
Sander Vrielink <sander.vrielink@gmail.com>
2019-11-28 20:05:52 +0100

    Cleaning up

---
+++ week_3/compare.c
@@ -0,0 +1,50 @@
+// Comapre 2 strings
+#include <stdio.h>
+#include <cs50.h>
+#include <string.h>
+
+// Declaring the function, otherwise c doesn't know the function exists
+// char * --> thats the pointer to the memory location
+// This is only by strings (char *)
+// a memory location ends with \0
+bool compare(char *a, char *b);
+
+int main(void)
+{
+    char *input1 = get_string("First string: ");
+    if (!input1)
+    {
+        return 1;
+    }
+    char *input2 = get_string("Second string: ");
+    if (!input2)
+    {
+        return 1;
+    }
+
+    if (compare(input1, input2))
+    {
+        printf("same\n");
+    }
+    else
+    {
+        printf("differemt\n");
+    }
+    
+}
+
+bool compare(char *a, char *b)
+{
+    if (strlen(a) != strlen(b))
+    {
+        return false;
+    }
+    for (int i = 1, s = strlen(a); i < s; i++)
+    {
+        if (a[i] != b[i])
+        {
+            return false;
+        }
+    }
+    return true;
+}
